88dc8eeaba1d28416933f9e6b0540ba2fba67ddf
e3rfh1 <3460285354@qq.com>
2020-02-25 17:25:20 +0800

    Site updated: 2020-02-25 17:25:18

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Mine",
+        artist: 'ILLENIUM / Phoebe Ryan',
+        url: 'http://music.163.com/song/media/outer/url?id=30780496.mp3',
+        cover: 'http://p1.music.126.net/94Zjhb6ibXN9jpIjRQDbUw==/7762552092459017.jpg?param=130y130',
+      },
+      {
+        name: 'Umbrella (Matte Remix)',
+        artist: 'Matte / Ember Island',
+        url: 'http://music.163.com/song/media/outer/url?id=518904426.mp3',
+        cover: 'http://p1.music.126.net/1LrtvH8EpKb5iHKR9qEU0Q==/109951163063843501.jpg?param=130y130',
+      },
+      {
+        name: 'Stronger',
+        artist: 'Kelly Clarkson',
+        url: 'http://music.163.com/song/media/outer/url?id=26569168.mp3',
+        cover: 'http://p2.music.126.net/NyN6he8m4t4F_yjOR_Tlww==/2495891395114271.jpg?param=130y130',
+      },
+      {
+        name: 'Into You',
+        artist: 'Matisse & Sadko / Hanne Mj?en',
+        url: 'http://music.163.com/song/media/outer/url?id=523042017.mp3',
+        cover: 'http://p1.music.126.net/NtNx7glQcmHfQPIiNYip6w==/109951163078910922.jpg?param=130y130',
+      },
+      {
+        name: 'Take me hand',
+        artist: 'DAISHI DANCE / Cecile Corbel',
+        url: 'http://music.163.com/song/media/outer/url?id=26092806.mp3',
+        cover: 'http://p2.music.126.net/AoVFMKkLVMOmTMo_A6x02g==/109951163403515653.jpg?param=130y130',
+      },
+    ]
+});
